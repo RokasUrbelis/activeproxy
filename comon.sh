77fd37147b07278666037aa406592ec702a4f8d2
北慕城南 <rokas.yang@gmail.com>
2021-02-28 13:51:25 +0800

    Update comon.sh

--- comon.sh
+++ comon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function getip() {
-i=0;while :;do let i++;var=$(curl --stderr /dev/null ifconfig.co)&>/dev/null &&echo -e "[$(date "+%H:%M:%S")|IP info]Your IP${i}:\033[32m$var\033[0m" && sleep 9.5;done;
+i=0;while :;do let i++;var=$(curl --stderr /dev/null ident.me)&>/dev/null &&echo -e "[$(date "+%H:%M:%S")|IP info]Your IP${i}:\033[32m$var\033[0m" && sleep 9.5;done;
 }
 export http_proxy='http://127.0.0.1:8118'
 export https_proxy='https://127.0.0.1:8118'
